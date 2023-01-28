01cc46e212c248a1007474f5dc58e9ddadd6147e
Peter Seale <peter@pseale.com>
2023-01-28 12:37:01 -0600

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,93 @@
+-- this file was created while watching ThePrimeagen's excellent video guide
+-- I've made major changes, notably shoving everything in one file, and also omitting several things
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.cmd [[packadd packer.nvim]]
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "rust", "lua", "vim", "help", "toml", "yaml", "javascript" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+vim.cmd.colorscheme 'melange'
+
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+lsp.setup()
+lsp.ensure_installed({
+    'rust_analyzer',
+    'sumneko_lua'
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior=cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+vim.opt.nu = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.backup = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+vim.g.mapleader = " "
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use "savq/melange-nvim"
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+end)
+
+
