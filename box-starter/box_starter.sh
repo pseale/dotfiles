746d8e2185e1e079c430bc9a7cdc10e23ac92d72
Peter Seale <peter@pseale.com>
2023-03-31 14:11:52 -0500

    Create box_starter.sh

---
+++ box-starter/box_starter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'copy/paste, don't actually run this'
+exit 1
+
+
+echo 'It says to put those commands in the .bashrc config:'
+
+echo 'shopt -s histappend'
+echo 'PROMPT_COMMAND="history -a;$PROMPT_COMMAND"'
+
