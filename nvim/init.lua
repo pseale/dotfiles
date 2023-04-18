5c1c25112ff71265489a1ab39f2e4d4ae3c26dc9
Peter Seale <peter@pseale.com>
2023-04-17 22:12:12 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader><leader>", "<cmd>lua vim.lsp.buf.hover()<cr>")
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = { "rust", "lua", "vim", "vimdoc", "toml", "yaml", "javascript" },
